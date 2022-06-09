0661597e4f10cf58cecf23476551cbaca4879aa0
Michael Binder <michael.binder@scheppach.com>
2022-06-10 01:47:20 +0200

    changed example; changed readme

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # dawui
 Simple dart web ui framework inspired by flutter
+
+## Example
+```dart
+import 'package:dawui/dawui.dart';
+
+void main() {
+  runApp((Map<String, String> args) => MainApp());
+}
+
+class MainApp extends Widget {
+  final counter = Observable(0);
+
+  @override
+  Widget build() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        // Both work the same
+        counter.observe((_) => Text("Counter: ${counter.value}")),
+        counter.observe((value) => Text("Counter: $value")),
+        SizedBox(height: "8px"),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ColoredButton(
+              child: counter.observe((value) => Text("Clicked $value times")),
+              onPressed: (Widget sender) {
+                counter.value += 1;
+              },
+            ),
+          ],
+        ),
+        SizedBox(height: "16px"),
+        Container(
+          width: "80%",
+          child: Row(
+            children: [
+              Flexible(
+                flex: 2,
+                child: AccentButton(child: Text("Flexible 1"), width: "100%"),
+              ),
+              Flexible(
+                flex: 1,
+                child: ColoredButton(child: Text("Flexible 2"), width: "100%"),
+              ),
+              Flexible(
+                flex: 2,
+                child: AccentButton(child: Text("Flexible 3"), width: "100%"),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+```
+
+![example.png](./readme/example1.png)
--- example/web/main.dart
+++ example/web/main.dart
@@ -5,62 +5,47 @@ void main() {
 }
 
 class MainApp extends Widget {
-  final btnText1 = Observable("Button 1");
-  final btnTextA = Observable("A");
-  var textField = TextField(label: "Hello", value: "World", onChange: (sender) => print((sender as TextField).value));
-  var button1 = ColoredButton(child: Text("Hii"), onPressed: (sender) => (sender as ColoredButton).width = "100%");
-  var awesomeButton = ColoredButton(
-      height: "120px",
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("X"),
-          SizedBox(width: "8px"),
-          Text("Hii"),
-        ],
-      ),
-      onPressed: (sender) => (sender as ColoredButton).width = "100%");
+  final counter = Observable(0);
+
   @override
   Widget build() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        awesomeButton,
-        button1,
-        textField,
-        ColoredButton(child: Text("Textfield"), onPressed: (_) => textField.value = "Pennor"),
-        btnText1.observe((_) => ColoredButton(
-            child: Text(btnText1.value), onPressed: (_) => btnText1.value == "Button 1" ? btnText1.value = "Button 2" : btnText1.value = "Button 1")),
-        btnTextA.observe(
-            (_) => ColoredActionButton(child: Text(btnTextA.value), onPressed: (_) => btnTextA.value == "A" ? btnTextA.value = "B" : btnTextA.value = "A")),
+        // Both work the same
+        counter.observe((_) => Text("Counter: ${counter.value}")),
+        counter.observe((value) => Text("Counter: $value")),
+        SizedBox(height: "8px"),
         Row(
-          crossAxisAlignment: CrossAxisAlignment.baseline,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(label: "Test 2"),
-            ColoredButton(child: Text("Button 2")),
+            ColoredButton(
+              child: counter.observe((value) => Text("Clicked $value times")),
+              onPressed: (Widget sender) {
+                counter.value += 1;
+              },
+            ),
           ],
         ),
-        Row(
-          children: [
-            TextField(label: "Test 3"),
-            ColoredButton(child: Text("Button 3")),
-          ],
-        ),
-        Row(
-          children: [
-            Flexible(
-              flex: 2,
-              child: AccentButton(child: Text("Button 4"), width: "100%"),
-            ),
-            Flexible(
-              flex: 1,
-              child: ColoredButton(child: Text("Button 5"), width: "100%"),
-            ),
-            Flexible(
-              flex: 3,
-              child: AccentButton(child: Text("Button 6"), width: "100%"),
-            ),
-          ],
+        SizedBox(height: "16px"),
+        Container(
+          width: "80%",
+          child: Row(
+            children: [
+              Flexible(
+                flex: 2,
+                child: AccentButton(child: Text("Flexible 1"), width: "100%"),
+              ),
+              Flexible(
+                flex: 1,
+                child: ColoredButton(child: Text("Flexible 2"), width: "100%"),
+              ),
+              Flexible(
+                flex: 2,
+                child: AccentButton(child: Text("Flexible 3"), width: "100%"),
+              ),
+            ],
+          ),
         ),
       ],
     );
